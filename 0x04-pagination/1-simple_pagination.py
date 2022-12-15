470df467f0b03db946422cd5e303960ae0ccf3e1
Luke Hackett <luke-hackett@outlook.com>
2022-12-15 16:42:54 +1100

    Simple pagination

---
+++ 0x04-pagination/1-simple_pagination.py
@@ -0,0 +1,52 @@
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        '''
+        Gets the start of the page and the end of the page.
+        '''
+        assert type(page) == int
+        assert type(page_size) == int
+        assert page > 0
+        assert page_size > 0
+        pagination = self.index_range(page, page_size)
+        dataset = self.dataset()
+
+        try:
+            parsed_dataset = dataset[pagination[0]:pagination[1]]
+        except Exception as e:
+            return []
+
+        return parsed_dataset
+
+    def index_range(self, page, page_size):
+        '''
+        Returns a tuple of the size of start of the index and end of the inndex
+        pages are 1-indexed.
+        '''
+
+        end_of_index = page * page_size
+        start_of_index = end_of_index - page_size
+
+        return (start_of_index, end_of_index)
