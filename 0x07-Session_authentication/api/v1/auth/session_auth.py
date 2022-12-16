4f8072a4175d66e00c65ba99de5f23761e3d767c
Luke Hackett <luke-hackett@outlook.com>
2022-12-16 16:29:28 +1100

    return user_id based on session id

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -23,3 +23,11 @@ class SessionAuth(Auth):
 
         self.user_id_by_session_id.update({session_id: user_id})
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        '''
+        Returns the User ID based on the session ID.
+        '''
+        if not isinstance(session_id, str) or session_id is None:
+            return None
+        return self.user_id_by_session_id.get(session_id)
