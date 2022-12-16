49a26a18e4a4a34e57810b685ca7edd0086ee88e
Luke Hackett <luke-hackett@outlook.com>
2022-12-16 16:14:09 +1100

    add session auth class.

---
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+Class inherits from auth.
+'''
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    '''
+    Inherits from auth class.
+    '''
+    pass
