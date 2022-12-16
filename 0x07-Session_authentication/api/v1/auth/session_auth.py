b7971a6d1de8f19909b790eb146e4f2e7b5b687a
Luke Hackett <luke-hackett@outlook.com>
2022-12-16 16:26:24 +1100

    create session_id and user_id_by_session id

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -4,10 +4,22 @@ Class inherits from auth.
 '''
 
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     '''
     Inherits from auth class.
     '''
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        '''
+        Creates a sessionID for a UserID.
+        '''
+        if not isinstance(user_id, str) or user_id is None:
+            return None
+        session_id = str(uuid.uuid4())
+
+        self.user_id_by_session_id.update({session_id: user_id})
+        return session_id
