d8d36ab59d5a91ab1b5fbb32ea97587d96f0aa28
Luke Hackett <luke-hackett@outlook.com>
2022-12-16 15:59:59 +1100

    add current user to request.

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -62,6 +62,7 @@ def load_auth():
     if auth.authorization_header(request) is None:
         abort(401)
 
+    request.current_user = auth.current_user(request)
     if auth.current_user(request) is None:
         abort(403)
 
