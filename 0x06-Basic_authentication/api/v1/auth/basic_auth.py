37fac017eb2931c4f9f588348eed6a6ceac592a9
Luke Hackett <luke-hackett@outlook.com>
2022-12-16 14:52:26 +1100

    create extract function for BasicAuth class.

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,4 +10,19 @@ class BasicAuth(Auth):
     '''
     Inherits from auth class.
     '''
-    pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        '''
+        Checks the authorisation header for Basic string to encode in Base64.
+        '''
+        if authorization_header is None:
+            return None
+
+        if not isinstance(authorization_header, str):
+            return None
+
+        if not authorization_header.startswith('Basic '):
+            return None
+
+        return authorization_header[6:]
