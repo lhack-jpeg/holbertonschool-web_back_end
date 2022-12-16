a00d58ce7191b595e7f4e57bd4315f3b1b80533e
Luke Hackett <luke-hackett@outlook.com>
2022-12-16 07:39:43 +0000

    add check for password and set session

--- 0x07-Session_authentication/api/v1/views/session_auth.py
+++ 0x07-Session_authentication/api/v1/views/session_auth.py
@@ -3,7 +3,7 @@
 """
 
 from api.v1.views import app_views
-from flask import abort, jsonify, request
+from flask import abort, jsonify, request, make_response
 from models.user import User
 
 
@@ -11,14 +11,39 @@ from models.user import User
                  methods=['POST'],
                  strict_slashes=False)
 def session_auth_form():
+    print(request.form)
     email = request.form.get('email')
+    print(email)
     password = request.form.get('password')
 
     if email is None:
-        abort(400, jsonify({"error": "email missing"}))
+        err_msg = jsonify({"error": "email missing"})
+        response = make_response(err_msg, 400)
+        abort(response)
     if password is None:
-        abort(400, jsonify({"error": "password missing"}))
+        err_msg = jsonify({"error": "password missing"})
+        response = make_response(err_msg, 400)
+        abort(response)
 
     users = User.search({'email': email})
     print(users)
-    return jsonify({}), 200
+    if len(users) == 0:
+        err_msg = jsonify({"error": "no user found for this email"})
+        response = make_response(err_msg, 404)
+        abort(response)
+
+    if password == _user.is_valid_password(password):
+        from api.v1.app import auth
+        for user in users:
+            session_id = auth.create_session(_user.id)
+            session_name = getenv('SESSION_NAME')
+            response = make_respone(user.to_json())
+            response.set_cooke(session_name, session_id)
+            return response
+
+   
+    err_msg = jsonify({ "error": "wrong password" })
+    response = make_response(err_msg, 401)
+    abort(response)
+
+    
